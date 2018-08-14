d961a1c1f2dfd1c05498294d951bee688b9be1a2
Monsieur19 <42003367+Monsieur19@users.noreply.github.com>
2018-08-15 00:35:38 +0300

    Добавил изображения

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var sliderCount=document.getElementsByTag("items-1","input");
+var listItem=document.querySelectorAll("best-item");
+
+var checkInput=function(){
+	for(var i=0;i<3;i++){
+		if(sliderCount[i].checked){
+			listItem[i].style.display=block;
+			return 1;
+		}
+		else{
+			listItem[i].style.display=none;
+			return i;
+		}
+	}
+}
